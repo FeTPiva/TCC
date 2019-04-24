23b0897c3ec88a7fa2ddbb8abf0c717ec76c28ac
Fernanda Piva <fernanda.piva@la.logicalis.com>
2019-04-24 18:52:38 -0300

    sdhgf

--- testsFerPy/tests1/frequencia.py
+++ testsFerPy/tests1/frequencia.py
@@ -4,8 +4,10 @@ import string
 num_words = 0
 frequency = {}
 document_text = open('teste.txt', 'r')  # chamada do doc
-text_string = document_text.read().lower() # pega o conteudo do txt e transforma tudo em string com lower case
-match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string) # delimitação do tamanho das palavras q for querer
+# pega o conteudo do txt e transforma tudo em string com lower case
+text_string = document_text.read().lower()
+# delimitação do tamanho das palavras q for querer
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
 for word in match_pattern:
     count = frequency.get(word, 0)
@@ -15,10 +17,17 @@ frequency_list = frequency.keys()
 
 for words in frequency_list:
     print(words, frequency[words])
-
+palavra = input("Enter word to be searched:")
+#palavra = [palavras...]
 with open('teste.txt', 'r') as f:
     for line in f:
         words = line.split()
         num_words += len(words)
+        k = 0
+      #pt pra filtrar a palavra
+       #for j -> palavra.leght, i==palavra[j]
+       for i in words:
+            if(i == palavra):
+                k = k+1
 print("Number of words:", num_words)
-
+print("Occurrences of the word:", k)
