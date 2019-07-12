1db78532a0c8810a0cacd9d71972f9a952b9c3a1
fefe <fetpiva@gmail.com>
2019-07-11 21:19:38 -0300

    dfhajdf

--- contagem_de_palavras/main.py
+++ contagem_de_palavras/main.py
@@ -1,1 +1,21 @@
-shgghg
+import contagem
+import re
+import string
+
+textoAnalisado = 'analisando.txt'
+pronome = 'pronomes.txt'
+absoluta = 'absoluta.txt'
+triste = 'triste.txt'
+
+# chamada do doc
+with open(textoAnalisado, 'r') as document_text:
+    minhaString = document_text.read()
+
+#pronomes (refazer)
+contagem.contandoCoisas(minhaString, pronome)
+
+#palavras absolutas (comletar mais)
+contagem.contandoCoisas(minhaString, absoluta)
+
+#contagem triste (ok)
+contagem.contandoCoisas(minhaString, triste)
--- testsFerPy/contagem_especificas/main.py
+++
@@ -1,9 +0,0 @@
-import pronomes
-import absolutas
-import depre
-
-
-
-frequencia.calcFrequencia('analisando.txt')
-
-print(frequencia.retornoFrequencia('analisando.txt'))
