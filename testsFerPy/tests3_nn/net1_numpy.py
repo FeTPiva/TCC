4d4ab5c17ce15d022731639dfdfe699a623da8bc
fefs trabalhadora <fernanda.piva@la.logicalis.com>
2019-07-24 16:25:44 -0300

    afgsg

--- testsFerPy/tests3_nn/net1_numpy.py
+++ testsFerPy/tests3_nn/net1_numpy.py
@@ -1,7 +1,7 @@
 #https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 #https://gist.github.com/jamesloyys/ff7a7bb1540384f709856f9cdcdee70d#file-neural_network_backprop-py
 
-
+#ver2 pra xteste https://towardsdatascience.com/machine-learning-in-python-numpy-neural-network-in-9-steps-eafd0db25906
 # Imports
 #obs:numpy é um biblioteca pra coisas cientificas onde vc pode mexer c arrays de tamanho nnos seus valores(super matrizoes)
 import numpy as np 
