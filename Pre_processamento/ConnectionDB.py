87e65676554265a1eb9b20ef1f353493f332b320
fefe <fetpiva@gmail.com>
2019-09-12 08:57:33 -0300

    aifgbjf

--- Pre_processamento/ConnectionDB.py
+++ Pre_processamento/ConnectionDB.py
@@ -1,6 +1,15 @@
 import mysql.connector
 import Pre_processamento.Corretor as Corretor
 # Conexao com Banco de Dados
+import nltk
+
+nltk.download('punkt') #punkt e rslp
+
+def Tokenize(sentence):
+    sentence = sentence.lower()
+    sentence = nltk.word_tokenize(sentence)
+    return sentence
+
 
 mydb = mysql.connector.connect(
 host="localhost",
@@ -40,7 +49,7 @@ def obterLinhaFrase(idTexto):  #Vinicius
 	return data
 
 
-def obterLinhaTexto(idPessoa):  #Fernanda
+def obterLinhaTexto(idPessoa):  #Fernanda - versao antiga
 	data = []	
 	mycursor = mydb.cursor()
 	mycursor.execute("SELECT GROUP_CONCAT(texto SEPARATOR ', '), isDepressivo FROM textodepressao WHERE idPessoa= " + str(idPessoa)) 
@@ -54,7 +63,7 @@ def obterLinhaTexto(idPessoa):  #Fernanda
 		data.append(jsonData)
 	return data
 
-#retorno geral de textos limitando por ntextos, por cada pessoa
+#retorno geral de textos limitando por ntextos, por cada pessoa v1
 def retornaNTextos(idPessoa, nTextos):
 		
 	mycursor = mydb.cursor()
@@ -69,7 +78,7 @@ def retornaNTextos(idPessoa, nTextos):
 		data.append(jsonData)
 	return data
 
-
+#retorna todos os textos da pessoa juntos v1
 def retornaTextosPorPessoa(idPessoa, nTextos): 
 	data = []	
 	mycursor = mydb.cursor()
@@ -79,6 +88,57 @@ def retornaTextosPorPessoa(idPessoa, nTextos):
 		x_correto = Corretor.correct_phrase(x[0])
 		jsonData = {
 			"texto": x_correto,
+			"isDepressivo": x[1]
+		}
+		data.append(jsonData)
+	return data
+
+
+#retorna todos os textos da pessoa juntos v2 - pegando numero de palavras
+def retornaTextosPorPessoaToken(idPessoa, nTextos, nToken): 
+	data = []	
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT GROUP_CONCAT(texto SEPARATOR ', '), isDepressivo FROM textodepressao WHERE idPessoa= %s LIMIT %s ;"%(idPessoa,nTextos)) 
+	myresult = mycursor.fetchall()
+
+	for x in myresult:
+		x_correto = Corretor.correct_phrase(x[0])
+	
+		#pensar aki... vou acabar pegando tipo por ex 10 palavras texto, mas n é o q eu quero pro meu(contagem)	
+		frase = Tokenize(x_correto)
+		#size = len(frase)		
+		fraseFiltrada = []
+		i=0
+		while i < nToken:
+			fraseFiltrada.append(frase[i])
+			i+=1
+
+		jsonData = {
+			"texto": fraseFiltrada,
+			"isDepressivo": x[1]
+		}
+		data.append(jsonData)
+	return data
+
+
+#retorno geral de textos limitando por ntextos, por cada pessoa v2 - pegando nmr de palavras
+def retornaNTextosToken(idPessoa, nTextos, nToken):
+		
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT texto, isDepressivo FROM textodepressao WHERE idPessoa = %s LIMIT %s ;"%(idPessoa, nTextos)) 
+	myresult = mycursor.fetchall()
+	for x in myresult:
+		x_correto = Corretor.correct_phrase(x[0])
+		frase = Tokenize(x_correto)
+		#size = len(frase)		
+		fraseFiltrada = []
+		i=0
+		while i < nToken:
+			fraseFiltrada.append(frase[i])
+			i+=1
+
+		jsonData = {
+			"texto": fraseFiltrada,
 			"isDepressivo": x[1]
 		}
 		data.append(jsonData)
