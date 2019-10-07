98fb5a376bb17ab77ed66bfb066a267ca2ee6319
Felipe Araujo <felipe_seiya96@hotmail.com>
2019-10-07 00:08:19 -0300

    Flask versão 0.5 rs

--- app.py
+++ app.py
@@ -1,63 +1,58 @@
 from flask import Flask, request, jsonify
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
+import mysql.connector
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential
+import os
+
+db_buffer = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root",
+  database="depressao"
 )
 
-users = [
-{
-    "user": "fefe",
-    "password": "fefe"
-},
-{
-    "user": "dridri",
-    "password": "dridri"
-}
-]
-lista = [
-    {
-        "id":10,
-        "nome":"Farofa",
-        "preco":12.50
-    },
-    {
-        "id":3, 
-        "nome":"batata",
-        "preco":200
-    }
-]
-#eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1Njg0NjI1NzQsIm5iZiI6MTU2ODQ2MjU3NCwianRpIjoiZWQ0YmUyNWQtNzBiMC00YWVhLWIwMjYtOWFmYWU3OTZmYjA3IiwiZXhwIjoxNTY4NDYzNDc0LCJpZGVudGl0eSI6ImZlZmUiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.aYC3GdNCkxizmR7MaIi6lXmM7JqdUbzShVIxBvwZm0I
+print(tf.__version__)
+print(keras.__version__)
+print(os.path.join(os.path.dirname(__file__),'modelDepre.h5'))
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = 'dri123'
-jwt = JWTManager(app)
 
-@app.route('/auth', methods = ['POST'])
-def auth():
-    retorno = {"message":"Credencias inválidas!"}, 401
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    for i in users:
-        if (i["user"] == username) and (i["password"] == password):
-            access_token = create_access_token(identity=username)
-            retorno = jsonify(access_token=access_token), 200
-    return retorno
-            
+# Retorno 
+#{
+#    "frases": [
+#        "",
+#        "",
+#        "",
+#        ...
+#    ]
+#}
 
-@app.route('/produtos', methods=['GET'])
-#@jwt_required
-def get_produtos():
-    prod_list = {"items" : lista}
-    return jsonify(prod_list)
-
-@app.route('/produto/<int:id>', methods=['GET'])
-#@jwt_required
+@app.route('/depressao', methods=['POST'])
 def get_produto():
-    prod = {"id": "nome"}
-    for produto in lista:
-        if produto["id"] == id:
-            prod = produto
-    return jsonify(prod)
-    
+    i = 1
+    erro = ""
+    status_code = 200
+    data = request.get_json()
+    mycursor = db_buffer.cursor()
+    drop = "TRUNCATE TABLE tbl_xteste"
+    mycursor.execute(drop)
+    db_buffer.commit()
+    sql = "INSERT INTO tbl_xteste (idTexto,idPessoa,texto) VALUES (%s,1,%s)"
+    if data["frases"].len() > 0 and data["pessoa"] != '':
+        for frase in data["frase"]:
+            val = (i,frase, data["pessoa"])
+            mycursor.execute(sql, val)
+            db_buffer.commit()
+            print("Dados inseridos com sucesso!\n")
+            i += 1
+    else:
+        erro = {"Message": "Erro ao ler JSON... Algum atributo está inválido!"}
+        status_code = 400
+        return jsonify(erro), status_code
+    model = keras.models.load_model(os.path.join(os.path.dirname(__file__),'modelTiago.h5'))
+    y_pred = model.predict(data["frase"])
+    print(y_pred)
+
 if __name__ == '__main__':
     app.run(debug=True)
