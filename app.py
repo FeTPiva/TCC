8279ccf2b16a67074338d2f75b28a8fd9a021765
Felipe Araujo <felipe_seiya96@hotmail.com>
2019-10-08 22:58:40 -0300

    Flask 0.8 rsrsrs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, Sequential
 import os
+import numpy as np
 
 db_buffer = mysql.connector.connect(
   host="localhost",
@@ -14,7 +15,7 @@ db_buffer = mysql.connector.connect(
 
 print(tf.__version__)
 print(keras.__version__)
-print(os.path.join(os.path.dirname(__file__),'modelDepre.h5'))
+print(os.path.join(os.path.dirname(__file__),'modelTiago.h5'))
 app = Flask(__name__)
 app.debug = True
 
@@ -39,9 +40,9 @@ def get_produto():
     mycursor.execute(drop)
     db_buffer.commit()
     sql = "INSERT INTO tbl_xteste (idTexto,idPessoa,texto) VALUES (%s,1,%s)"
-    if data["frases"].len() > 0 and data["pessoa"] != '':
-        for frase in data["frase"]:
-            val = (i,frase, data["pessoa"])
+    if len(data["frases"]) > 0:
+        for frase in data["frases"]:
+            val = (i,frase)
             mycursor.execute(sql, val)
             db_buffer.commit()
             print("Dados inseridos com sucesso!\n")
@@ -50,9 +51,12 @@ def get_produto():
         erro = {"Message": "Erro ao ler JSON... Algum atributo está inválido!"}
         status_code = 400
         return jsonify(erro), status_code
-    model = keras.models.load_model(os.path.join(os.path.dirname(__file__),'modelTiago.h5'))
-    y_pred = model.predict(data["frase"])
-    print(y_pred)
-
+    model = keras.models.load_model(os.path.join(os.path.dirname(__file__),'models/modelTiago.h5'))
+    frases = data["frases"]
+    #Pré processamento, ver com a Fe amanhã
+    x_pred = np.array()
+    y_pred = model.predict()
+    print(x_pred.shape)
+    return {"msg" : "sucesso"},200
 if __name__ == '__main__':
     app.run(debug=True)
