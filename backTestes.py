0d86fafe60ccbd54e2a184d767daa9a4f4dcdf3f
FeTPiva <fetpiva@gmail.com>
2019-10-24 02:41:00 -0300

    ajsfas

--- backTestes.py
+++ backTestes.py
@@ -9,7 +9,7 @@ import contagem_de_palavras.polaridade as polaridade
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
-
+ 
 def geraVetorResultados():
     #pega a função predefinida no banco de dds e ai retorna o vetor tratado
     
@@ -40,26 +40,19 @@ def geraVetorResultados2(vetor):
     contagem_palavras = contagem.retornaContagemTeste2(vetor)
 
     sentimentos7 = mineracaoemocao7.retorna_Votacao_Emocao_Probabilidade_Por_Media_GeralTeste2(vetor)
-    #print(sentimentos7) 
-
-
-    sentimentos3 = mineracao_emocao3.retornaVetorProbTeste2(0)
-    #print(sentimentos3) #ok
-
-
-    sentimentos3v2 = mineracao_emocao3.retornaVetor001Teste2(0)
-    #print(sentimentos3v2) #ok
-
-    pol = polaridade.polarizandoTeste2(0)
-    #print(pol)
     
+    sentimentos3 = mineracao_emocao3.retornaVetorProbTeste2(vetor)
+    
+    sentimentos3v2 = mineracao_emocao3.retornaVetor001Teste2(vetor)
+    
+
+    pol = polaridade.polarizandoTeste2(vetor)
+        
     #juntando a macaronada
     probs_teste = contagem.criaLista(contagem_palavras[0], contagem_palavras[1], contagem_palavras[2], sentimentos7[0], sentimentos7[1],sentimentos7[2],sentimentos7[3],sentimentos7[4],sentimentos7[5], sentimentos3[0], sentimentos3[1], sentimentos3[2],sentimentos3v2[0],sentimentos3v2[1],sentimentos3v2[2], pol)
 
     #print(probs_teste)
     return probs_teste
-
-
 
 
 def predictKeras(vetor):
